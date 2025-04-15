d2b88886b5a19dcc19bac96aa7854235102f46f7
Cyrus C <97295877+CyrusCKF@users.noreply.github.com>
2025-04-15 17:41:16 +0800

    stream output

--- src/git_gen/generate_commit.py
+++ src/git_gen/generate_commit.py
@@ -1,11 +1,22 @@
+from queue import Queue
 import subprocess
+from threading import Thread
 
 import torch
-from transformers.utils.quantization_config import BitsAndBytesConfig
 from transformers.models.auto.modeling_auto import AutoModelForCausalLM
 from transformers.models.auto.tokenization_auto import AutoTokenizer
-from transformers.pipelines.text_generation import TextGenerationPipeline
+from transformers.models.qwen2.modeling_qwen2 import Qwen2ForCausalLM
 from transformers.pipelines import pipeline
+from transformers.pipelines.text_generation import TextGenerationPipeline
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase
+from transformers.utils.quantization_config import BitsAndBytesConfig
+from transformers.generation.streamers import (
+    TextStreamer,
+    TextIteratorStreamer,
+    BaseStreamer,
+)
+
+from transformers.generation.configuration_utils import GenerationConfig
 
 instruction = """You are Git Commit Message Pro, a specialist in crafting precise, professional Git commit messages from .diff files. Your role is to analyze these files, interpret the changes, and generate a clear, direct commit message.
 
@@ -16,15 +27,55 @@ Guidelines:
 4. If you cannot read the entire file, attempt to generate a message based on the available information."""
 
 
-def generate_commit():
+class OutputStream(BaseStreamer):
+    STOP_SIGNAL = None
 
-    quantization_config = BitsAndBytesConfig(
-        load_in_4bit=True, bnb_4bit_compute_dtype=torch.bfloat16
+    def __init__(self) -> None:
+        super().__init__()
+        self.output_queue = Queue()
+        self.next_tokens_are_prompt = True
+
+    def put(self, value):
+        """
+        Receives tokens and push them to queue
+        """
+        if self.next_tokens_are_prompt:
+            self.next_tokens_are_prompt = False
+            return
+        self.output_queue.put(value)
+
+    def end(self):
+        self.output_queue.put(self.STOP_SIGNAL)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        value = self.output_queue.get()
+        if value == self.STOP_SIGNAL:
+            raise StopIteration()
+        else:
+            return value
+
+
+def generate_commit():
+    tokenizer: PreTrainedTokenizerBase = AutoTokenizer.from_pretrained(
+        "Qwen/Qwen2.5-Coder-7B-Instruct"
     )
-    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-Coder-7B-Instruct")
-    model = AutoModelForCausalLM.from_pretrained(
-        "CyrusCheungkf/git-commit-7B-q4b", quantization_config=quantization_config
-    )
+    if torch.cuda.is_available():
+        print("CUDA detected. Using quantized model")
+        quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True, bnb_4bit_compute_dtype=torch.bfloat16
+        )
+        model: Qwen2ForCausalLM = AutoModelForCausalLM.from_pretrained(
+            "CyrusCheungkf/git-commit-7B-q4b",
+            quantization_config=quantization_config,
+        )
+    else:
+        print("CUDA not detected. Using a weaker model")
+        model: Qwen2ForCausalLM = AutoModelForCausalLM.from_pretrained(
+            "CyrusCheungkf/git-commit-1.5B"
+        )
 
     result = subprocess.run(["git", "diff"], capture_output=True, text=True)
     result.check_returncode()
@@ -33,19 +84,21 @@ def generate_commit():
         {"role": "user", "content": instruction + "\n\nInputs:\n" + git_diff},
     ]
 
-    # tokens = (
-    #     torch.tensor(tokenizer.apply_chat_template(conversation)).unsqueeze(0).cuda()
-    # )
-    # next_token = None
-    # end_id = tokenizer.convert_tokens_to_ids(tokenizer.eos_token)
-    # while next_token != end_id:
-    #     with torch.no_grad():
-    #         logits = model(tokens).logits
-    #         next_token = torch.argmax(logits[:, -1], dim=-1, keepdim=True)
-    #     tokens = torch.cat((tokens, next_token), dim=1)
-    #     print(tokenizer.decode(next_token.item()), end="")
-    pipe = pipeline(
-        "text-generation", model=model, device_map="auto", tokenizer=tokenizer
+    tokens = tokenizer.apply_chat_template(
+        conversation, add_generation_prompt=True, return_tensors="pt"
     )
-    outputs = pipe(conversation, num_return_sequences=4, return_full_text=False)
-    return outputs
+    streamer = OutputStream()
+    generation_kwargs = dict(
+        inputs=tokens,
+        streamer=streamer,
+        num_return_sequences=4,
+        pad_token_id=tokenizer.eos_token_id,
+        max_length=2048,
+        # temperature=0.1,
+        # top_p=0.8,
+        # repetition_penalty=1.25,
+    )
+    thread = Thread(target=model.generate, kwargs=generation_kwargs)
+    thread.start()
+    for output in streamer:
+        print([tokenizer.decode(o) for o in output], flush=True)
